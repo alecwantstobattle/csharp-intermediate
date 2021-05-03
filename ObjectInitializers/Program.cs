e16fda5ec92e90041f1fcf3426b3372fb71c2e59
Alec Tecson <tecsonalr@gmail.com>
2021-05-03 18:05:12 +0800

    Object Initializers

---
+++ ObjectInitializers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                FirstName = "Mosh",
+                LastName = "Hamedani"
+            };
+        }
+    }
+}
