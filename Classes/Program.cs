6d95cc2fdc0026924b28395d8646095aafef9c58
Alec Tecson <tecsonalr@gmail.com>
2021-05-03 17:46:15 +0800

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        public class Person
+        {
+            public string Name;
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hi {0}, I am {1}", to, Name);
+            }
+
+            //public Person Parse(string str)
+            public static Person Parse(string str)
+            {
+                var person = new Person();
+                person.Name = str;
+
+                return person;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //var person = new Person();
+            //person.Name = "John";
+            //person.Introduce("Mosh");
+
+            //var person = new Person();
+            //var p = person.Parse("John");
+
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
+        }
+    }
+}
