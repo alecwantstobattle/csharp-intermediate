285309f549ccc477839bb2381f2e1b204b95ae06
Alec Tecson <tecsonalr@gmail.com>
2021-05-05 13:28:10 +0800

    Boxing and Unboxing

---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Mosh");
+            list.Add(DateTime.Today);
+
+            var number = (int)list[1]; // invalid cast
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add()
+        }
+    }
+}
