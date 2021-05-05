4b1aeb28bac20c7bb15b5bc57bbd48e6c2e6e358
Alec Tecson <tecsonalr@gmail.com>
2021-05-05 20:01:38 +0800

    Abstract Classes and Members

---
+++ AbstractClassesAndMembers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var shape = new Shape(); // cannot create abstract classes
+
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+}
