c58f495ee3aebe92469a1f5a7b37e91592e01d02
Alec Tecson <tecsonalr@gmail.com>
2021-05-05 22:00:08 +0800

    Sealed Classes and Members

---
+++ SealedClassesAndMembers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClassesAndMembers
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+    }
+
+    //public sealed class Circle: Shape // cannot create a class that derive from circle
+    public class Circle: Shape
+    {
+        public sealed override void Draw() // can only be applied to methods that are overriding a virtual method in a base class
+        {
+            Console.WriteLine("Drawing a circle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
